dc7d2ab1080e78c3ea4ea24708c6b76f6fe4f92c
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-09-19 11:32:42 +0700

    student query 1

---
+++ student-management-query-1.sql
@@ -0,0 +1,24 @@
+use QuanLySinhVien;
+select * from student
+where StudentName Like 'H%';
+
+select * from class
+where month(StarDate) = 12;
+
+select * from subjects
+where Creadit between 3 and 5;
+
+update student
+set ClassId = 2
+where StudentId = 1;
+
+select S.StudentName,Sub.SubName,M.Mark
+from Student S join Mark M on S.StudentId = M.StudentId join Subjects Sub on M.SubId = Sub.SubId
+
+-- where M.mark in(
+-- select S.StudentName,Sub.SubName,M.Mark
+-- from Student S join Mark M on S.StudentId = M.StudentId join Subjects Sub on M.SubId = Sub.SubId
+-- having count(M.mark) > 1
+-- )
+order by M.mark desc;
+
