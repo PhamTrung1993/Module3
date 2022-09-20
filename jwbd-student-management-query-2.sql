283875ea12a0b6b20d790eac3f61f1cd3e0a5655
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-09-20 14:09:30 +0700

    student query 2

---
+++ jwbd-student-management-query-2.sql
@@ -0,0 +1,19 @@
+use quanlysinhvien;
+
+select Address, count(StudentId) as ' Số lượng học viên '
+from Student
+group by Address;
+
+select S.StudentId,S.StudentName,avg(Mark)
+from student S join mark M on S.StudentId = M.StudentId
+group by S.StudentId, S.StudentName;
+
+select S.StudentId,S.StudentName,avg(Mark)
+from student S join mark M on S.StudentId = M.StudentId
+group by S.StudentId, S.StudentName
+having avg(mark) > 15;
+
+select S.StudentId,S.StudentName,avg(Mark)
+from student S join mark M on S.StudentId = M.StudentId
+group by S.StudentId, S.StudentName
+having avg(mark) >= all(select avg(mark) from mark group by mark.StudentId);
